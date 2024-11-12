c8ecd39b542a9dc49128b6fd6ef94c1c9e29d71c
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2024-11-12 07:53:48 +0530

    added changes

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,9 +1,24 @@
 "use strict" //treat all the code as newer version of JavaScript
 // once activated the strict mode can't be deactivated in the whole file and its by default set use strict mode.
 
+// Premitive Datatype
 
 let name = 'Anurag' //string datatype
 let age = '20' //integer datatype
 let isLoggedIn = false //boolean datatype
 
-// the integer falls
+// the integer falls in range of 2^53 it is treated as integer and beyond that it is treated as bigint
+
+// null is standalone value and also a datatype. lets take an example, you are creating a temperature app which asks form server about the temperature, now there is some problem in the server and it can't send temp so you will send null as an value.
+
+// undefined is a datatype assigned when the values are not given to variables
+
+// symbols => is used to define the uniqueness of something. we will discuss later
+
+
+console.log(typeof 'hitesh')
+console.log(typeof null) // data type of null when asked with typeof gives object
+console.log(typeof undefined)
+
+
+
---
+++ 03_conversionsOperations.js
@@ -0,0 +1,25 @@
+// let score = '33abc'
+
+// console.log(typeof score)
+// console.log(typeof (score)) //here the typeof is used as method
+
+// let value = Number(score)
+// console.log(typeof(value))
+// console.log(value) //here it will return NaN which will be given as number in JavaScript
+
+
+// // null when converted to number gives 0
+// // undefined when converted to number gives NaN
+// // true when converted to number gives 1
+// // false when converted to number gives 0
+
+// // if the string is not convertible then it will be depicted as NaN
+
+
+// // for converting from one datatype to another , we use Number String Boolean Object
+
+// *********************** Operations ********************
+
+let value = 3
+let Negvalue = -value // here it might be a method 
+// console.log(Negvalue)
