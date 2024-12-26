6e79213d8cc58810715e2232ad8124332f0d8114
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2024-12-26 11:41:49 +0530

    learned while and do while loop

---
+++ 20_while.js
@@ -0,0 +1,13 @@
+let it = 0
+while (it <= 10) {
+    console.log(it)
+    it += 1
+}
+console.log('this is it:',it);
+
+
+let count = 0
+do {
+    console.log(count)
+    count += 1
+} while (count==10); //loop runs atleast once even if the condition is false
