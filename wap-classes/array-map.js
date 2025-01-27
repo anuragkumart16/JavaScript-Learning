50d93e90a2113c3a3940cd4aa34b22e345bd35cc
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-01-27 10:08:18 +0530

    Added changes

--- wap-classes/array-map.js
+++ wap-classes/array-map.js
@@ -1,3 +1,18 @@
 const myArray = [1, 2, 3, 'four', 'five', 6, 7, 'eight', 9, 10];
 console.log(myArray);
 
+function square(element ,index){
+    return element * element;
+}
+
+const squaredArray = myArray.map(square);
+console.log(squaredArray.filter(element => isNaN(element) == false))
+
+const element = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(element.map((element,index)=>{
+    if (element%2==0){
+        return 'Even'
+    }else{
+        return 'Odd'
+    }
+}))
