ae72b88ecc8198334e248ced38c874840f68606c
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-01-27 09:24:44 +0530

    learned new items

---
+++ wap-classes/array-includes.js
@@ -0,0 +1,5 @@
+const array = new Array(1, 2, 3, 4, 5,'4');
+
+console.log(array.includes('4')); // true
+console.log(array.includes('4',2)); // this will check after the second index
+
---
+++ wap-classes/array-map.js
@@ -0,0 +1,3 @@
+const myArray = [1, 2, 3, 'four', 'five', 6, 7, 'eight', 9, 10];
+console.log(myArray);
+
