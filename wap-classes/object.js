5d6442e2a92dbd9983294459a143ec95b409abaf
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-01-29 10:37:27 +0530

    learned the objects

---
+++ wap-classes/object.js
@@ -0,0 +1,19 @@
+let smartphone = {
+    price : 20000,
+    ram : '8gb',
+    model : '2010',
+    color: 'black',
+    storage : '64gb',
+    camera : '12mp',
+    battery : '4000mah',
+    os : 'android',
+    processor : 'snapdragon 8',
+    1: 'hehe'
+}
+console.log(smartphone)
+
+
+let array = [{ department: 'IT' , employee: 'Anurag',salary: 20000}, { department: 'HR', employee: 'Kumar',salary: 30000 }, { department: 'Finance' , employee: 'Tiwari',salary: 40000}];
+
+console.log(array)
+console.log(smartphone['1'])
